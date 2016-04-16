4fd6cedf5357a2447591d958d87519a1ff6c466a
Calvin-Glein <glenn_matias@dlsu.edu.ph>
2016-04-17 00:04:55 +0800

    Nakakapag lipat na nag files DELETES THE OLD PICTURE

--- INTROSE/src/Product/Picturemover.java
+++ INTROSE/src/Product/Picturemover.java
@@ -4,10 +4,18 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import javax.swing.UIManager;
+
 public class Picturemover {
-	public Picturemover(File from, File to) {
+
+	public Picturemover(String path) {
+
+		 File from = new File(path);
+
+	 File to = new File("C:\\School\\INTROSE\\INTROSE Github\\SourceCodes\\INTROSE\\Pictures\\" + from.getName());
+	
+	 String p = to.getAbsolutePath().toString();
 		int nCopy = 0;
-		String p = to.getAbsolutePath().toString();
 		String d = "";
 		String ex = "";
 
@@ -31,9 +39,11 @@ public class Picturemover {
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
+
+		deleteFile(from);
 	}
 
-	private static void deleteFile(File from) {
+	public void deleteFile(File from) {
 		try {
 			Files.delete(from.toPath());
 		} catch (IOException e) {
@@ -41,50 +51,46 @@ public class Picturemover {
 		}
 	}
 
-	private static void moveFile(File from, File to) {
-		copyFile(from, to);
-		deleteFile(from);
-	}
 }
 
-////ORIGINAL COPY PASTE FROM PAT
-//private static void copyFile(File from, File to) {
-//	int nCopy = 0;
-//	String p = to.getAbsolutePath().toString();
-//	String d = "";
-//	String ex = "";
+//// ORIGINAL COPY PASTE FROM PAT
+// private static void copyFile(File from, File to) {
+// int nCopy = 0;
+// String p = to.getAbsolutePath().toString();
+// String d = "";
+// String ex = "";
 //
-//	try {
-//		while (to.exists()) {
-//			nCopy++;
-//			for (int i = p.length() - 1; i >= 0; i--) {
-//				if (p.charAt(i) == '.') {
-//					if (nCopy == 1) {
-//						d = p.substring(0, i);
-//						ex = p.substring(i);
-//						p = p.substring(0, i) + " - Copy" + p.substring(i);
-//					} else {
-//						p = d + " - Copy (" + nCopy + ")" + ex;
-//					}
-//					to = new File(p);
-//				}
-//			}
-//		}
-//		Files.copy(from.toPath(), to.toPath());
-//	} catch (IOException e) {
-//		System.out.println(e.getMessage());
-//	}
-//}
+// try {
+// while (to.exists()) {
+// nCopy++;
+// for (int i = p.length() - 1; i >= 0; i--) {
+// if (p.charAt(i) == '.') {
+// if (nCopy == 1) {
+// d = p.substring(0, i);
+// ex = p.substring(i);
+// p = p.substring(0, i) + " - Copy" + p.substring(i);
+// } else {
+// p = d + " - Copy (" + nCopy + ")" + ex;
+// }
+// to = new File(p);
+// }
+// }
+// }
+// Files.copy(from.toPath(), to.toPath());
+// } catch (IOException e) {
+// System.out.println(e.getMessage());
+// }
+// }
 //
-//private static void deleteFile(File from) {
-//	try {
-//		Files.delete(from.toPath());
-//	} catch (IOException e) {
-//		System.out.println(e.getMessage());
-//	}
-//}
+// private static void deleteFile(File from) {
+// try {
+// Files.delete(from.toPath());
+// } catch (IOException e) {
+// System.out.println(e.getMessage());
+// }
+// }
 //
-//private static void moveFile(File from, File to) {
-//	copyFile(from, to);
-//	deleteFile(from);
-//}
+// private static void moveFile(File from, File to) {
+// copyFile(from, to);
+// deleteFile(from);
+// }
